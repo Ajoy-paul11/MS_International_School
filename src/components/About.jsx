493bae66e893ae539a0c280145aba6321959717b
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-01-30 18:29:23 +0530

    Refactor Programs, ProgramCard, and About components for improved readability and styling consistency.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import AboutImage from "../assets/MS_International.png"
+import React from "react";
+import AboutImage from "../assets/MS_International.png";
 
 function About() {
   return (
     <div>
-        <div className=' bg-[#F3F4F6] text-[#4B5563] py-8 font-inter'>
-            <h2 className=' text-[#1E3A8A] text-3xl lg:text-4xl text-center font-bold p-4 my-4'>About</h2>
-            <section className=' flex flex-col lg:flex-row justify-evenly items-center xl:items-start bg-cover bg-center px-6'>
-                <div className=' w-full lg:w-1/2 '>
-                    <img src={AboutImage} alt="Scholl-image" 
-                    className=' rounded-tr-[50px] rounded-bl-[50px]'/>
-                </div>
-                <h3 className=' text-[#4B5563] w-full lg:w-1/2 text-base xl:text-xl 2xl:text-3xl font-semibold p-4 pl-8 xl:pl-16'>
-                MS International, Mandya is a Co-educational School, founded in 2017 under the chairmanship of Sri. M Srinivas.
-                Located in Hanekere, Mandya.
-                <p className=' my-4 indent-4'>
-                MS International Public School. The only school in Mandya to provide facilities with all the international standards. remains committed to providing an academically rigorous education to students who will walk out of school ready for lives of leadership and service to their community. From Literacy to music and arts, each day at MS International is filled with activities that are both enriching and fun.
-                </p>
-                <p className=' indent-4'>
-                We strive to be committed to the students we are privileged to serve.
-                </p>
-                </h3>
-            </section>
-        </div>
+      <div className=" bg-[#F3F4F6] text-[#4B5563] py-8 font-inter">
+        <h2 className=" text-[#1E3A8A] text-3xl lg:text-4xl text-center font-bold p-4 my-4">
+          About
+        </h2>
+        <section className=" flex flex-col lg:flex-row justify-evenly items-center xl:items-start bg-cover bg-center px-6">
+          <div className=" w-full lg:w-1/2 ">
+            <img
+              src={AboutImage}
+              alt="Scholl-image"
+              className=" rounded-tr-[50px] rounded-bl-[50px]"
+            />
+          </div>
+          <h3 className=" text-[#4B5563] w-full lg:w-1/2 text-base xl:text-xl 2xl:text-3xl font-semibold p-4 pl-8 xl:pl-16">
+            MS International, Mandya is a Co-educational School, founded in 2017
+            under the chairmanship of Sri. M Srinivas. Located in Hanekere,
+            Mandya.
+            <p className=" my-4 indent-4">
+              MS International Public School. The only school in Mandya to
+              provide facilities with all the international standards. remains
+              committed to providing an academically rigorous education to
+              students who will walk out of school ready for lives of leadership
+              and service to their community. From Literacy to music and arts,
+              each day at MS International is filled with activities that are
+              both enriching and fun.
+            </p>
+            <p className=" indent-4">
+              We strive to be committed to the students we are privileged to
+              serve.
+            </p>
+          </h3>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react'
-import kidImage from "../assets/kid.png"
+import React, { useState, useEffect, useRef } from "react";
+import kidImage from "../assets/kid.png";
 import { PiStudentFill } from "react-icons/pi";
 import { FaChalkboardTeacher } from "react-icons/fa";
 import { SiGoogleclassroom } from "react-icons/si";
-import CountUp from 'react-countup';
-
+import CountUp from "react-countup";
 
 function HeroSection() {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,30 +12,30 @@ function HeroSection() {
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
           setIsVisible(true);
           observer.disconnect();
         }
       },
       { threshold: 0.5 }
     );
-     
-    if(containerRef.current) {
+
+    if (containerRef.current) {
       observer.observe(containerRef.current);
     }
 
     return () => {
-      if(containerRef.current) {
+      if (containerRef.current) {
         observer.unobserve(containerRef.current);
       }
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     // bg-gradient-to-r from-[#0D9488] to-[#0891B2]
 
-    <div className=' relative animate-gradient bg-[length:400%_400%] overflow-hidden font-inter'
-    ><style>{`
+    <div className=" relative animate-gradient bg-[length:400%_400%] overflow-hidden font-inter">
+      <style>{`
         @keyframes gradient {
           0% {
             background-position: 0% 50%;
@@ -62,45 +61,57 @@ function HeroSection() {
           animation: gradient 10s ease infinite;
         }
       `}</style>
-        <div>
-            <div className=' xl:container xl:mx-auto md:mx-5 flex justify-start items-center p-4 h-[90vh] '>
-                <div className='text-white mt-[-330px] lg:mt-0 z-10'>
-                    <h1 className=' text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-bold '>Welcome to <br /> <span className='text-[#D97706]'>  MS INTERNATIONAL </span><br /> <span className='text-[#D97706]'>PUBLIC SCHOOL</span> </h1>
-                    <p className='text-base lg:text-xl mt-2 md:mt-4 text-white'>" Viveka Vidhya Samsthe "</p>
-                </div>  
-            </div>
+      <div>
+        <div className=" xl:container xl:mx-auto md:mx-5 flex justify-start items-center p-4 h-[90vh] ">
+          <div className="text-white mt-[-330px] lg:mt-0 z-10">
+            <h1 className=" text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-bold ">
+              Welcome to <br />{" "}
+              <span className="text-[#D97706]"> MS INTERNATIONAL </span>
+              <br /> <span className="text-[#D97706]">PUBLIC SCHOOL</span>{" "}
+            </h1>
+            <p className="text-base lg:text-xl mt-2 md:mt-4 text-white">
+              " Viveka Vidhya Samsthe "
+            </p>
+          </div>
         </div>
-        <div className=' absolute top-[12%] lg:top-[20%] right-[-35px] md:right-[10px] lg:right-[20px] 2xl:right-[10%] z-10'>
-            <img src={kidImage} alt="" className=' mix-blend-normal w-[200px] lg:w-[400px] '/>
+      </div>
+      <div className=" absolute top-[12%] lg:top-[20%] right-[-35px] md:right-[10px] lg:right-[20px] 2xl:right-[10%] z-10">
+        <img
+          src={kidImage}
+          alt=""
+          className=" mix-blend-normal w-[200px] lg:w-[400px] "
+        />
+      </div>
+      <div
+        className=" absolute top-[320px] left-0 right-0 lg:relative lg:top-0 xl:p-4"
+        ref={containerRef}
+      >
+        <div className=" container mx-auto flex flex-col lg:flex-row justify-evenly items-center gap-4 lg:gap-4 bg-[#f4f5ff] p-4 xl:rounded">
+          <div className=" flex flex-col justify-center items-center gap-2 line-violet">
+            <PiStudentFill className=" w-8 h-8 text-[#1E3A8A]" />
+            <h3 className=" font-bold text-2xl lg:text-4xl text-[#4B5563]">
+              {isVisible && <CountUp start={1} end={450} duration={4} />}+
+            </h3>
+            <p className=" font-semibold text-base">Current Enrollment</p>
+          </div>
+          <div className=" flex flex-col justify-center items-center gap-2 line-teal">
+            <SiGoogleclassroom className=" w-8 h-8 text-[#0D9488]" />
+            <h3 className=" font-bold text-2xl lg:text-4xl text-[#4B5563]">
+              {isVisible && <CountUp start={1} end={24} duration={4} />}+
+            </h3>
+            <p className=" font-semibold text-base">Current Classroom</p>
+          </div>
+          <div className=" flex flex-col justify-center items-center gap-2 line-gold">
+            <FaChalkboardTeacher className=" w-8 h-8 text-[#D97706]" />
+            <h3 className=" font-bold text-2xl lg:text-4xl text-[#4B5563]">
+              {isVisible && <CountUp start={1} end={50} duration={4} />}+
+            </h3>
+            <p className=" font-semibold text-base">Qualified Teachers</p>
+          </div>
         </div>
-        <div className=' absolute top-[320px] left-0 right-0 lg:relative lg:top-0 xl:p-4' ref={containerRef}>
-            <div className=' container mx-auto flex flex-col lg:flex-row justify-evenly items-center gap-4 lg:gap-4 bg-[#f4f5ff] p-4 xl:rounded'>
-              <div className=' flex flex-col justify-center items-center gap-2 line-violet'>
-               <PiStudentFill className=' w-8 h-8 text-[#1E3A8A]'/>
-                <h3 className=' font-bold text-2xl lg:text-4xl text-[#4B5563]'>
-                  { isVisible && <CountUp start={1} end={450} duration={4}/>}+
-                  </h3>
-                <p className=' font-semibold text-base'>Current Enrollment</p>
-              </div>
-              <div className=' flex flex-col justify-center items-center gap-2 line-teal'>
-                <SiGoogleclassroom className=' w-8 h-8 text-[#0D9488]'/>
-                <h3 className=' font-bold text-2xl lg:text-4xl text-[#4B5563]'>
-                  {isVisible && <CountUp start={1} end={24} duration={4}/>}+
-                  </h3>
-                <p className=' font-semibold text-base'>Current Classroom</p>
-              </div>
-              <div className=' flex flex-col justify-center items-center gap-2 line-gold'>
-                <FaChalkboardTeacher className=' w-8 h-8 text-[#D97706]'/>
-                <h3 className=' font-bold text-2xl lg:text-4xl text-[#4B5563]'>
-                  {isVisible && <CountUp start={1} end={50} duration={4}/>}+
-                  </h3>
-                <p className=' font-semibold text-base'>Qualified Teachers</p>
-              </div>
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
-
+export default HeroSection;
