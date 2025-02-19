4097910da7ffa94b42d0e29ea4749ed5e7ff4567
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-02-19 13:55:25 +0530

    update CulturalEvent and OtherSchoolEvent components; replace background image and add multiple event images with enhanced styling.

--- src/components/CulturalEvent.jsx
+++ src/components/CulturalEvent.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-import bgImage from "../assets/bg-image.jpg";
+import bgImage from "../assets/bg-image2.png";
+import cultureImage1 from "../assets/culturealevent/cultural1.jpg";
+import cultureImage2 from "../assets/culturealevent/cultural2.jpg";
+import cultureImage3 from "../assets/culturealevent/cultural3.jpg";
+import cultureImage4 from "../assets/culturealevent/cultural4.jpg";
+import cultureImage5 from "../assets/culturealevent/cultural5.jpg";
+import cultureImage6 from "../assets/culturealevent/cultural6.jpg";
+import cultureImage7 from "../assets/culturealevent/cultural7.jpg";
+import cultureImage8 from "../assets/culturealevent/cultural8.jpg";
+import cultureImage9 from "../assets/culturealevent/cultural9.jpg";
+import cultureImage10 from "../assets/culturealevent/cultural10.jpg";
+import cultureImage11 from "../assets/culturealevent/cultural11.jpg";
+import cultureImage12 from "../assets/culturealevent/cultural12.jpg";
 
 function CulturalEvent() {
   return (
@@ -21,9 +33,46 @@ function CulturalEvent() {
             </div>
           </div>
         </div>
-        <div>
-            {/* Images would come */}
+        <div className="  bg-color grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 xl:gap-10 p-8 lg:px-12 xl:px-16 ">
+          {/* Images would come */}
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage1} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage2} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage3} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage4} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage5} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage6} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage7} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage8} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage9} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage10} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage11} alt="culture-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={cultureImage12} alt="culture-image" className="" />
+          </div>
         </div>
+        <hr className=" border-color" />
       </div>
     </div>
   );
--- src/components/OtherSchoolEvent.jsx
+++ src/components/OtherSchoolEvent.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import bgImage from "../assets/bg-image.jpg";
+import bgImage from "../assets/bg-image2.png";
+import event1 from "../assets/otherevent/event1.jpg";
+import event2 from "../assets/otherevent/event2.jpg";
+import event3 from "../assets/otherevent/event3.jpg";
+import event4 from "../assets/otherevent/event4.jpg";
+import event5 from "../assets/otherevent/event5.jpg";
+import event6 from "../assets/otherevent/event6.jpg";
+import event7 from "../assets/otherevent/event7.jpg";
+import event8 from "../assets/otherevent/event8.jpg";
+import event9 from "../assets/otherevent/event9.jpeg";
+import event10 from "../assets/otherevent/event10.jpeg";
 
 function OtherSchoolEvent() {
   return (
@@ -21,9 +31,40 @@ function OtherSchoolEvent() {
             </div>
           </div>
         </div>
-        <div>
-            {/* Images would come */}
+        <div className="  bg-color grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 xl:gap-10 p-8 lg:px-12 xl:px-16 ">
+          {/* Images would come */}
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event1} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event2} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event3} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event4} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event5} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event6} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event7} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event8} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event9} alt="event-image" className="" />
+          </div>
+          <div className=" bg-color rounded-lg overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+            <img src={event10} alt="event-image" className="" />
+          </div>
         </div>
+        <hr className=" border-color" />
       </div>
     </div>
   );
