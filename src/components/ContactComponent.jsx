066c7902bb0bd63a13bc08dd292f95444b934db6
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-03-19 12:43:23 +0530

    add Form component for contact submissions; update favicon and improve ContactComponent layout; add .env to .gitignore

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -92,7 +92,7 @@ export default function ContactComponent() {
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                   </svg>
-                  <span className=" text-lg md:text-xl xl:text-2xl">
+                  <span className=" text-sm md:text-xl xl:text-2xl">
                     msinternationalpublicschool2@gmail.com
                   </span>
                 </p>
@@ -118,7 +118,7 @@ export default function ContactComponent() {
             </div>
 
             {/* Contact Form Section */}
-            <div className="w-full bg-color primary-text-color p-6 rounded-lg shadow-md">
+            <div className="w-[350px]md:w-full bg-color primary-text-color p-6 rounded-lg shadow-md">
               <h2 className="  text-center text-xl lg:text-3xl font-bold mb-4">
                 Contact Us
               </h2>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import banner2 from "../assets/banner/banner2.jpg";
 import banner3 from "../assets/banner/banner3.jpg";
 import { FaArrowUp } from "react-icons/fa";
 import { FaArrowDown } from "react-icons/fa";
+import Form from "./Form";
 
 function HeroSection() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -16,28 +17,21 @@ function HeroSection() {
   ];
 
   const nextSlide = () => {
-    // setSlideDirection("next");
     setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
   };
 
   const prevSlide = () => {
-    // setSlideDirection("prev");
     setCurrentSlide((prev) => (prev === 0 ? images.length - 1 : prev - 1));
   };
 
   // Auto-slide every 5 seconds
   useEffect(() => {
-    // const timer = setInterval(() => {
-    //   setSlideDirection("next");
-    //   setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-    // }, 5000);
     const timer = setInterval(nextSlide, 5000);
 
     return () => clearInterval(timer);
   }, [nextSlide]); 
 
   return (
-    // <div className="relative w-full h-[300px] sm:h-[400px] md:h-[500px] lg:[600px] overflow-hidden">
     <div className="relative w-full h-full overflow-hidden">
     
       {/* Images */}
@@ -50,13 +44,11 @@ function HeroSection() {
         {images.map((image, index) => (
           <div
             key={index}
-            // className={`w-full h-full flex-shrink-0 transition-transform duration-500 `}
             className={`w-full h-full flex-shrink-0 `}
           >
             <img
               src={image || "/placeholder.svg"}
               alt={`Slide ${index + 1}`}
-            //   className="w-full h-full object-cover"
               className="w-full h-full object-contain"
             />
           </div>
@@ -67,7 +59,6 @@ function HeroSection() {
       <div className="absolute left-6 top-1/2 -translate-y-1/2 flex flex-col gap-10 z-10">
         <button
           onClick={prevSlide}
-        //   className="p-2 rounded-full bg-white/80 hover:bg-white shadow-lg transition-colors"
         className=" mb-2"
           aria-label="Previous slide"
         >
@@ -86,12 +77,14 @@ function HeroSection() {
         </div>
         <button
           onClick={nextSlide}
-        //   className="p-2 rounded-full bg-white/80 hover:bg-white shadow-lg transition-colors"
         className=" mt-2"
           aria-label="Next slide"
         >
           <FaArrowDown className="h-4 lg:h-5 w-4 lg:w-5 text-white" />
         </button>
+      </div>
+      <div className="absolute top-[15%] right-6 z-10 w-[360px]">
+        <Form />
       </div>
     </div>
   );
